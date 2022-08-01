e8021224bc068c8b2cc62b76b908cb7567d3a46a
Johan Morales <jhellmetal2000@gmail.com>
2022-07-31 23:43:46 -0500

    fixed some broken routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,10 +5,10 @@ import { AboutComponent } from './components/about/about.component';
 import {RickSearchComponent} from "./components/rick-search/rick-search.component";
 
 const APP_ROUTES: Routes = [
-  { path: 'home', component: HomeComponent, redirectTo: 'rickSearch'},
+  { path: 'home', component: HomeComponent},
   { path: 'rickSearch', component: RickSearchComponent},
   { path: 'about', component: AboutComponent },
-  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+  { path: '**', pathMatch: 'full', redirectTo: 'rickSearch' }
 ];
 
 export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES, {useHash:true});
